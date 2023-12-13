a705a8d1c1482796328ca9ccea6ba05e0206bf6a
Nirina Rakotoniary <nirinamercia@mail.com>
2023-12-13 16:07:21 +0100

    project 1

--- src/projects/e_emploi/Eemploi.jsx
+++ src/projects/e_emploi/Eemploi.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import {Box, Typography} from "@mui/material";
+import EemploiPict from "../../pict/e_emp_pic.png";
 
 const Eemploi = () => {
     return (
         <Box className="w-full p-4">
-            <Box sx={{borderTop:"0.5px solid blue"}} className="h-96 flex gap-10 items-end p-10 border rounded-2xl bg-gradient-to-t from-blue-300 to-gray-50">
-                <Box className={"w-1/2 fill-amber-200"}>
+            <Box sx={{borderTop:"0.5px solid blue"}} className="flex flex-col-reverse md:flex-row items-center md:justify-between gap-10 p-10 border rounded-2xl bg-gradient-to-t from-blue-300 to-gray-50">
+                <Box className="md:w-1/2">
                     <Box>
-                        <Typography variant="h4" sx={{marginBottom:"20px"}} ><span className={"font-manrope"}>E-Emploi project</span></Typography>
-                        <Typography><span className={"text-xl"}>A website, as our Final Year Project, in order to make one who are looking a tasker to do a task and the tasker in contact</span></Typography>
+                        <Typography variant="h4" sx={{marginBottom:"20px"}} ><span className={"font-manrope text-center"}>E-Emploi project</span></Typography>
+                        <Typography><span className={"text-xl text-center"}>A website, as our Final Year Project, in order to make one who are looking for a tasker to do a task and the tasker in contact</span></Typography>
                     </Box>
                 </Box>
-                <Box>Droite</Box>
+                <Box className="md:w-1/2 flex justify-center">
+                    <img src={EemploiPict} alt="picture e_emploi" className="md:w-full w-1/2" />
+                </Box>
             </Box>
         </Box>
     )
